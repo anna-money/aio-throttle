192c8d454c2a2b8dfeb8941e747df955900fa7ba
Yury Pliner <yury.pliner@gmail.com>
2019-09-15 15:59:52 +0500

    Split tests to multiple files

---
+++ tests/test_throttle_capacity_and_queue_limit.py
@@ -0,0 +1,61 @@
+import logging
+import time
+from asyncio import sleep, gather
+from collections import Counter
+
+import pytest
+
+from aio_throttle import Throttler
+
+DELAY = 1
+SUCCEED = "+"
+FAILED = "-"
+
+logger = logging.getLogger(__name__)
+
+
+class Server:
+    def __init__(self, delay, throttler):
+        self.throttler = throttler
+        self.delay = delay
+
+    async def handle(self, consumer=None):
+        async with self.throttler.throttle(consumer) as result:
+            logger.debug(self.throttler.stats)
+            if not result:
+                return FAILED
+            await sleep(self.delay)
+            return SUCCEED
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "capacity_limit, queue_limit, succeed_count, failed_count, multiplier",
+    [
+        (1, 0, 1, 0, 1),
+        (1, 0, 1, 1, 1),
+        (2, 0, 2, 0, 1),
+        (2, 0, 2, 1, 1),
+        (1, 1, 2, 0, 2),
+        (1, 1, 2, 1, 2),
+        (2, 1, 3, 0, 2),
+        (2, 1, 3, 1, 2),
+        (1, 1, 2, 0, 2),
+        (1, 1, 2, 1, 2),
+        (1, 2, 3, 0, 3),
+        (1, 2, 3, 1, 3),
+    ],
+)
+async def test(capacity_limit, queue_limit, succeed_count, failed_count, multiplier):
+    server = Server(DELAY, Throttler(capacity_limit, queue_limit))
+
+    handle_tasks = list(map(lambda x: server.handle(), range(0, succeed_count + failed_count)))
+
+    start = time.monotonic()
+    statuses = await gather(*handle_tasks)
+    end = time.monotonic()
+
+    counter = Counter(statuses)
+    assert counter[SUCCEED] == succeed_count
+    assert counter[FAILED] == failed_count
+    assert multiplier * DELAY <= end - start <= (1.1 * multiplier * DELAY)
--- tests/test_throttle_consumer.py
+++ tests/test_throttle_consumer.py
@@ -11,6 +11,8 @@ from aio_throttle import Throttler
 DELAY = 1
 SUCCEED = "+"
 FAILED = "-"
+FIRST_CONSUMER = "first"
+SECOND_CONSUMER = "second"
 
 logger = logging.getLogger(__name__)
 
@@ -27,39 +29,6 @@ class Server:
                 return FAILED
             await sleep(self.delay)
             return SUCCEED
-
-
-@pytest.mark.asyncio
-@pytest.mark.parametrize(
-    "capacity_limit, queue_limit, succeed_count, failed_count, multiplier",
-    [
-        (1, 0, 1, 0, 1),
-        (1, 0, 1, 1, 1),
-        (2, 0, 2, 0, 1),
-        (2, 0, 2, 1, 1),
-        (1, 1, 2, 0, 2),
-        (1, 1, 2, 1, 2),
-        (2, 1, 3, 0, 2),
-        (2, 1, 3, 1, 2),
-        (1, 1, 2, 0, 2),
-        (1, 1, 2, 1, 2),
-        (1, 2, 3, 0, 3),
-        (1, 2, 3, 1, 3),
-    ],
-)
-async def test_simple_workload(capacity_limit, queue_limit, succeed_count, failed_count, multiplier):
-    server = Server(DELAY, Throttler(capacity_limit, queue_limit))
-
-    handle_tasks = list(map(lambda x: server.handle(), range(0, succeed_count + failed_count)))
-
-    start = time.monotonic()
-    statuses = await gather(*handle_tasks)
-    end = time.monotonic()
-
-    counter = Counter(statuses)
-    assert counter[SUCCEED] == succeed_count
-    assert counter[FAILED] == failed_count
-    assert multiplier * DELAY <= end - start <= (1.1 * multiplier * DELAY)
 
 
 @pytest.mark.asyncio
@@ -83,10 +52,6 @@ async def test_any_consumer_quota_workload(
     assert counter[SUCCEED] == succeed_count
     assert counter[FAILED] == failed_count
     assert multiplier * DELAY <= end - start <= (1.1 * multiplier * DELAY)
-
-
-FIRST_CONSUMER = "first"
-SECOND_CONSUMER = "second"
 
 
 @pytest.mark.asyncio
