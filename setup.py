5aff1d014a6164f9a52faa20120f172f92967f65
Yury Pliner <yury.pliner@gmail.com>
2019-10-28 23:26:37 +0500

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="aio-throttle",
-    version="1.0.0",
+    version="1.0.1",
     author="Yury Pliner",
     author_email="yury.pliner@gmail.com",
     description="A simple throttling package",
