4d637d2099819eda90744f4356792415bcd23a76
Yury Pliner <yury.pliner@gmail.com>
2021-03-11 00:22:59 +0500

    Drop default dict usage (#18)

---
+++ aio_throttle/utils.py
@@ -0,0 +1,21 @@
+from typing import TypeVar, Dict
+
+T = TypeVar('T')
+
+
+def increment_counter(dictionary: Dict[T, int], key: T) -> None:
+    value = dictionary.get(key)
+    if value is None:
+        dictionary[key] = 1
+    else:
+        dictionary[key] = value + 1
+
+
+def decrement_counter(dictionary: Dict[T, int], key: T) -> None:
+    value = dictionary.get(key)
+    if value is None:
+        return
+    if value - 1 == 0:
+        del dictionary[key]
+    else:
+        dictionary[key] = value - 1
